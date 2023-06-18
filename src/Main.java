4261beb4fcc1f95863643f09a00ad354e22bc7e7
Mihail Kislov <Mihael22831@gmail.com>
2023-06-19 01:10:31 +0300

    add new proiz void

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World - "+calcule.summ(4,4));
+        System.out.println("Hello World - "+calcule.proiz(4,4));
     }
 }
